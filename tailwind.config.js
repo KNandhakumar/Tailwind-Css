387dd515c200035ec2ef4429749493ed7d4b9b68
Nandhakumar <nknandhakumar64@gmail.com>
2024-06-05 12:49:34 +0530

    Typography and font size change and customize

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      osworld: ['Oswald','sans-serif'],
+      bebas: ['Bebas Neue','san-serifs']
+    },
+    fontSize: { // customize font size
+      sm: '0.3rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    }
   },
   plugins: [],
 }
